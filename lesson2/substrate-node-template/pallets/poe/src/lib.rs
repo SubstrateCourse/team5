2005eb6286304cfbb493f544366ba9fb65113722
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2020-06-06 17:03:23 +0800

    implement entry point create_claim

--- lesson2/substrate-node-template/pallets/poe/src/lib.rs
+++ lesson2/substrate-node-template/pallets/poe/src/lib.rs
@@ -9,7 +9,7 @@
 /// For more guidance on Substrate FRAME, see the example pallet
 /// https://github.com/paritytech/substrate/blob/master/frame/example/src/lib.rs
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, ensure, dispatch::{DispatchResult};
 use frame_system::{self as system, ensure_signed};
 
 #[cfg(test)]
@@ -39,20 +39,14 @@ decl_storage! {
 // The pallet's events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
-		/// Just a dummy event.
-		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
-		/// To emit this event, we call the deposit function, from our runtime functions
-		SomethingStored(u32, AccountId),
+		ClaimCreated(AccountId, Vec<u8>),
 	}
 );
 
 // The pallet's errors
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Value was None
-		NoneValue,
-		/// Value reached maximum and cannot be incremented further
-		StorageOverflow,
+		DuplicateClaim,
 	}
 }
 
@@ -69,38 +63,17 @@ decl_module! {
 		// this is needed only if you are using events in your pallet
 		fn deposit_event() = default;
 
-		/// Just a dummy entry point.
-		/// function that can be called by the external world as an extrinsics call
-		/// takes a parameter of the type `AccountId`, stores it, and emits an event
-		#[weight = 10_000]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let who = ensure_signed(origin)?;
+		#[weight = 0]
+		pub fn create_claim(origin, claim: Vec<u8>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
 
-			// Code to execute when something calls this.
-			// For example: the following line stores the passed in u32 in the storage
-			Something::put(something);
+			ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::DuplicateClaim);
+			
+			Proofs::<T>::insert(&claim, (sender.clone(), system::Module::<T>::block_number()));
 
-			// Here we are raising the Something event
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Self::deposit_event(RawEvent::ClaimCreated(sender, claim));
+
 			Ok(())
-		}
-
-		/// Another dummy entry point.
-		/// takes no parameters, attempts to increment storage value, and possibly throws an error
-		#[weight = 10_000]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			// Check it was signed and get the signer. See also: ensure_root and ensure_none
-			let _who = ensure_signed(origin)?;
-
-			match Something::get() {
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					Something::put(new);
-					Ok(())
-				},
-			}
 		}
 	}
 }
