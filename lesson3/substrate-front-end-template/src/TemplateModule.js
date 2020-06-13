992d78a860415696d4e9dc37ff98079151c8a2b3
mxkfz <etaifang@qq.com>
2020-06-13 19:35:24 +0800

    homework for lesson 3

--- lesson3/substrate-front-end-template/src/TemplateModule.js
+++ lesson3/substrate-front-end-template/src/TemplateModule.js
@@ -4,78 +4,124 @@ import { Form, Input, Grid, Card, Statistic } from 'semantic-ui-react';
 import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
 
+import { blake2AsHex } from '@polkadot/util-crypto';
+
 function Main (props) {
   const { api } = useSubstrate();
   const { accountPair } = props;
 
   // The transaction submission status
   const [status, setStatus] = useState('');
-
-  // The currently stored value
-  const [currentValue, setCurrentValue] = useState(0);
-  const [formValue, setFormValue] = useState(0);
+  const [digest, setDigest] = useState('');
+  const [owner, setOwner] = useState('');
+  const [blockNumber, setBlockNumber] = useState('');
+  const [dest, setDest] = useState('');
 
   useEffect(() => {
     let unsubscribe;
-    api.query.templateModule.something(newValue => {
-      // The storage value is an Option<u32>
-      // So we have to check whether it is None first
-      // There is also unwrapOr
-      if (newValue.isNone) {
-        setCurrentValue('<None>');
-      } else {
-        setCurrentValue(newValue.unwrap().toNumber());
-      }
+    api.query.poeModule.proofs(digest, (result) => {
+      setOwner(result[0].toString());
+      setBlockNumber(result[1].toNumber());
+
     }).then(unsub => {
       unsubscribe = unsub;
     })
       .catch(console.error);
 
     return () => unsubscribe && unsubscribe();
-  }, [api.query.templateModule]);
+  }, [digest, api.query.poeModule]);
+
+ const handleFileChosen = (file) => {
+    const fileReader = new FileReader();
+    const bufferToDigest = () => {
+      const content = Array.from(new Uint8Array(fileReader.result))
+        .map((b) => b.toString(16).padStart(2, '0'))
+        .join('');
+      const hash = blake2AsHex(content, 256);
+      setDigest(hash);
+    };
+    fileReader.onloadend = bufferToDigest;
+    fileReader.readAsArrayBuffer(file);
+  };
+
+  const onDestChange = (_, data) => {
+    setDest(data.value);
+  };
 
   return (
     <Grid.Column width={8}>
-      <h1>Template Module</h1>
-      <Card centered>
-        <Card.Content textAlign='center'>
-          <Statistic
-            label='Current Value'
-            value={currentValue}
-          />
-        </Card.Content>
-      </Card>
+      <h1>Proof fo existence Module</h1>
       <Form>
         <Form.Field>
           <Input
-            label='New Value'
-            state='newValue'
-            type='number'
-            onChange={(_, { value }) => setFormValue(value)}
+            type='file'
+            id='file'
+            lable='Your File'
+            onChange={(e) => handleFileChosen(e.target.files[0])}
           />
         </Form.Field>
-        <Form.Field style={{ textAlign: 'center' }}>
+        <Form.Field>
+          <Input
+            type='text'
+            label='To'
+            placeholder='address'
+            state='dest'
+            onChange={onDestChange}
+          />
+        </Form.Field>
+        <Form.Field>
           <TxButton
             accountPair={accountPair}
-            label='Store Something'
-            type='SIGNED-TX'
+            label='Create Claim'
             setStatus={setStatus}
-            attrs={{
-              palletRpc: 'templateModule',
-              callable: 'doSomething',
-              inputParams: [formValue],
-              paramFields: [true]
-            }}
+            type='SIGNED-TX'
+            attrs={
+              {
+                palletRpc: 'poeModule',
+                callable: 'createClaim',
+                inputParams: [digest],
+                paramFields: [true]
+              }
+            }
           />
-        </Form.Field>
-        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+          <TxButton
+            accountPair={accountPair}
+            label='Revoke Claim'
+            setStatus={setStatus}
+            type='SIGNED-TX'
+            attrs={
+              {
+                palletRpc: 'poeModule',
+                callable: 'revokeClaim',
+                inputParams: [digest],
+                paramFields: [true]
+              }
+            }
+          />
+          <TxButton
+            accountPair={accountPair}
+            label='Transfer Claim'
+            setStatus={setStatus}
+            type='SIGNED-TX'
+            attrs={
+              {
+                palletRpc: 'poeModule',
+                callable: 'transferClaim',
+                inputParams: [digest, dest],
+                paramFields: [true, true]
+              }
+            }
+          />          
+        </Form.Field>  
+        <div>{status}</div>
+        <div>{`Claim info, owner: ${owner}, blockNumber: ${blockNumber}`}</div>        
       </Form>
     </Grid.Column>
   );
 }
 
-export default function TemplateModule (props) {
+export default function PoeModule (props) {
   const { api } = useSubstrate();
-  return (api.query.templateModule && api.query.templateModule.something
+  return (api.query.poeModule && api.query.poeModule.proofs
     ? <Main {...props} /> : null);
 }
